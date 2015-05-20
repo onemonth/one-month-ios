0e3475a97b43a36b94264a58d5da790de565d303
Alfie Hanssen <alfie@One-Months-Mac-mini.local>
2015-05-19 21:21:39 -0400

    staffpicks: 1.6-tableview-setup, tableview displaying list of people

--- VimeoStaffPicks/VimeoStaffPicks/StaffPicksViewController.swift
+++ VimeoStaffPicks/VimeoStaffPicks/StaffPicksViewController.swift
@@ -8,14 +8,43 @@
 
 import UIKit
 
-class StaffPicksViewController: UIViewController {
+class StaffPicksViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView?
+    
+    let items = ["alfie", "lee", "mattan", "syd", "zach"]
+    
+    // MARK: View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
 
-        
+        self.setupTableView()
     }
+    
+    // MARK: Setup
+    
+    func setupTableView() {
+        
+        self.tableView?.registerClass(UITableViewCell.self, forCellReuseIdentifier: "placeholder")
+    }
+    
+    // MARK: UITableViewDatasource
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("placeholder") as! UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
 }
