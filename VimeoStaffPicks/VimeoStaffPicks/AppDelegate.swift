115cb74a3e09adbea8bdf8c334ee2ba97825afa9
Alfie Hanssen <alfie@One-Months-Mac-mini.local>
2015-05-19 20:43:12 -0400

    staffpicks: 1.5-uiviewcontroller-interface builder

--- VimeoStaffPicks/VimeoStaffPicks/AppDelegate.swift
+++ VimeoStaffPicks/VimeoStaffPicks/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        self.window?.rootViewController = StaffPicksViewController(nibName:"StaffPicksViewController", bundle:nil)
+        
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ VimeoStaffPicks/VimeoStaffPicks/StaffPicksViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StaffPicksViewController.swift
+//  VimeoStaffPicks
+//
+//  Created by Alfie Hanssen on 5/19/15.
+//  Copyright (c) 2015 Alfie Hanssen. All rights reserved.
+//
+
+import UIKit
+
+class StaffPicksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        
+    }
+
+}
