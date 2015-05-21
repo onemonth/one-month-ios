d83f1d1daa2f6f961108dfe80b7051b303e6bf8f
Alfie Hanssen <alfie@One-Months-Mac-mini.local>
2015-05-20 20:56:18 -0400

    1.13-getting response

--- VimeoStaffPicks/VimeoStaffPicks/VimeoClient.swift
+++ VimeoStaffPicks/VimeoStaffPicks/VimeoClient.swift
@@ -12,10 +12,41 @@ typealias ServerResponseCallback = (object: Dictionary<String,AnyObject>?, error
 
 class VimeoClient {
     
+    static let errorDomain = "VimeoClientErrorDomain"
+    static let baseURLString = "https://api.vimeo.com"
+    static let staffpicksPath = "/channels/staffpicks/videos"
+    static let authToken = "eeb3566316fc39f535a4276a63d90649"
+    
     class func staffpicks(callback: ServerResponseCallback) {
         
-        callback(object: nil, error: nil)
+        let URLString = baseURLString + staffpicksPath
+        var URL = NSURL(string: URLString)
         
+        if URL == nil {
+            
+            var error = NSError(domain: errorDomain, code: 0, userInfo: [NSLocalizedDescriptionKey : "Unable to create URL"])
+            callback(object: nil, error: error)
+            
+            return
+        }
+        
+        var request = NSMutableURLRequest(URL: URL!)
+        request.HTTPMethod = "GET"
+        request.addValue("Bearer " + authToken, forHTTPHeaderField: "Authorization")
+        
+        var task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+            
+            var responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            
+            println(response)
+            println(responseString)
+            
+            callback(object: nil, error: nil)
+
+            
+        })
+        
+        task.resume()
     }
     
 }
